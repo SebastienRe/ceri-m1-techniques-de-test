b4bb64e7db64d11704aa91676f762a99c52141ab
Sebastien <sebastien.re@alumni.univ-avignon.fr>
2024-02-06 18:15:21 +0100

    Ajout des classes de tests, mise à jour du POM

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -0,0 +1,22 @@
+package fr.univavignon.pokedex.api;
+
+import junit.framework.TestCase;
+
+public class IPokedexTest extends TestCase {
+
+    public void testSize() {
+
+    }
+
+    public void testAddPokemon() {
+    }
+
+    public void testGetPokemon() {
+    }
+
+    public void testGetPokemons() {
+    }
+
+    public void testTestGetPokemons() {
+    }
+}
